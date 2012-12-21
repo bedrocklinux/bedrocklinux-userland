a71207b40ba3cc0915495c419e77a862f5bca1ec
paradigm <paradigm@bedrocklinux.org>
2012-12-21 13:35:37 -0500

    get-bb: ignore comments

--- get-bb-cmds.sh
+++ get-bb-cmds.sh
@@ -7,8 +7,12 @@ cp -rp bedrocklinux-userland /tmp/get-bb-cmds-temp
 cd /tmp/get-bb-cmds-temp/
 make clean 1>/dev/null 2>/dev/null
 make remove-unnecessary 1>/dev/null 2>/dev/null
-find . -type f -exec awk '{for(i=1;i<=NF;i++){words[$i]++}}END{for(word in words){print word}}' > /tmp/get-bb-words {} \;
-sed 's/[^a-zA-Z0-9]/\n/g' /tmp/get-bb-words | sort | uniq > /tmp/get-bb-words-2
+
+for FILE in $(find . -type f)
+do
+	cat $FILE | sed -e 's/#.*$//g' -e 's/[^a-zA-Z0-9[]/\n/g' >> /tmp/get-bb-words
+done
+sort /tmp/get-bb-words | uniq > /tmp/get-bb-words-2
 
 cat > /tmp/get-bb-busyboxapplets <<EOF
 [, [[, acpid, addgroup, adduser, adjtimex, ar, arp, arping, ash,
