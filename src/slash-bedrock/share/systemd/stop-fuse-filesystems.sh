8ebfdde721b12390ee8b3355a9fa3fbccf96b8e2
Daniel Thau <danthau@bedrocklinux.org>
2022-01-10 07:04:46 -0500

    Adjust systemd unmount assistance version

--- src/slash-bedrock/share/systemd/stop-fuse-filesystems.sh
+++ src/slash-bedrock/share/systemd/stop-fuse-filesystems.sh
@@ -5,12 +5,12 @@
 # worked around this with a unit file that assists in unmounting Bedrock's FUSE
 # filesytems.
 #
-# Some systemd version circa systemd 247 in 2020 was found to have improved the
+# Some systemd version circa systemd 245 in 2020 was found to have improved the
 # FUSE filesystem handling, but changed something which resulted in Bedrock's
 # FUSE assistance causing a shutdown delay.
 #
-# Thus, we only want to assist unmounting for pre-247 systemd versions.
+# Thus, we only want to assist unmounting for pre-245 systemd versions.
 
-if command -v systemctl >/dev/null 2>&1 && [ "$(systemctl --version | awk '{print$2;exit}')" -lt 247 ]; then
+if command -v systemctl >/dev/null 2>&1 && [ "$(systemctl --version | awk '{print$2;exit}')" -lt 245 ]; then
 	umount -l /etc /bedrock/strata/*/etc /bedrock/cross /bedrock/strata/*/bedrock/cross
 fi
